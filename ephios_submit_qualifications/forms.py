720e5edc58c72d194ddf8581568fb709cc27433c
samuelson-ben <ben.samuelson@fiteka.de>
2025-07-24 14:01:32 +0200

    added form for expiration

--- ephios_submit_qualifications/forms.py
+++ ephios_submit_qualifications/forms.py
@@ -1,18 +1,18 @@
 from django.utils.translation import gettext_lazy as _
 from django import forms
 from ephios.core.models import Qualification
-from .models import QualificationRequest
+from .models import QualificationRequest, QualificationDefaultExpirationTime
 
 class QualificationSubmitForm(forms.ModelForm):
     qualification = forms.ModelChoiceField(
         queryset=Qualification.objects.all(),
         required=True,
-        label=_("Qualification:"),
+        label=_("Qualification"),
         widget=forms.Select(attrs={"placeholder": _("Select Qualification")})
     )
     qualification_date = forms.DateField(
         required=True,
-        label=_("Qualification Date:"),
+        label=_("Qualification Date"),
         widget=forms.DateInput(attrs={'type': 'date'})
     )
     image = forms.ImageField(
@@ -27,19 +27,16 @@ class QualificationSubmitForm(forms.ModelForm):
 
 class QualificationDetailForm(forms.Form):
     user = forms.CharField(
-        label=_("User"),
         required=True,
-        widget=forms.TextInput(attrs={'readonly': 'readonly'})
+        widget=forms.HiddenInput()
     )
     qualification = forms.CharField(
-        label=_("Qualification"),
         required=True,
-        widget=forms.TextInput(attrs={'readonly': 'readonly'})
+        widget=forms.HiddenInput()
     )
     requested_at = forms.DateTimeField(
         required=False,
-        label=_("Requested At"),
-        widget=forms.DateTimeInput(attrs={'type': 'datetime-local', 'readonly': 'readonly'})
+        widget=forms.HiddenInput()
     )
     qualification_date = forms.DateField(
         required=False,
@@ -53,6 +50,57 @@ class QualificationDetailForm(forms.Form):
     )
     reason = forms.CharField(
         required=False,
-        label=_("Reason for Action"),
-        widget=forms.TextInput(attrs={'placeholder': _('Optional reason for action')}),
+        label=_("Optional Reason for Action"),
+        widget=forms.TextInput(attrs={'placeholder': _('Reason')}),
     )
+    qualification_default_expiration_time_days = forms.IntegerField(
+        required=False,
+        widget=forms.HiddenInput(),
+    )
+    qualification_default_expiration_time_years = forms.IntegerField(
+        required=False,
+        widget=forms.HiddenInput(),
+    )
+
+class QualificationDefaultExpirationTimeAddForm(forms.ModelForm):
+    qualification = forms.ModelChoiceField(
+        queryset=Qualification.objects.all(),
+        required=True,
+        label=_("Qualification"),
+        widget=forms.Select(attrs={"placeholder": _("Select Qualification")})
+    )
+    default_expiration_time_years = forms.IntegerField(
+        required=True,
+        label=_("Years"),
+        widget=forms.NumberInput(attrs={'min': 0, 'placeholder': _("Years")})
+    )
+    default_expiration_time_days = forms.IntegerField(
+        required=True,
+        label=_("Days"),
+        widget=forms.NumberInput(attrs={'min': 0, 'placeholder': _("Days")})
+    )
+
+    class Meta:
+        model = QualificationDefaultExpirationTime
+        fields = ['qualification', 'default_expiration_time_years', 'default_expiration_time_days']
+
+class QualificationDefaultExpirationTimeDetailForm(forms.ModelForm):
+    qualification = forms.ModelChoiceField(
+        queryset=Qualification.objects.all(),
+        required=True,
+        widget=forms.HiddenInput()
+    )
+    default_expiration_time_years = forms.IntegerField(
+        required=True,
+        label=_("Years"),
+        widget=forms.NumberInput(attrs={'min': 0, 'placeholder': _("Years")})
+    )
+    default_expiration_time_days = forms.IntegerField(
+        required=True,
+        label=_("Days"),
+        widget=forms.NumberInput(attrs={'min': 0, 'placeholder': _("Days")})
+    )
+
+    class Meta:
+        model = QualificationDefaultExpirationTime
+        fields = ['qualification', 'default_expiration_time_years', 'default_expiration_time_days']
